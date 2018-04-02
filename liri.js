cb9954bbf9e5a5edda082f06a36ee069bd9f6077
abdiaziz jama <abdiazizjama@er-10-132-12-47.wireless.umn.edu>
2018-04-02 15:33:08 -0500

    2

--- liri.js
+++ liri.js
@@ -4,27 +4,70 @@ var Twitter = require("twitter");
 var Spotify = require("node-spotify-api");
 var req = require("request");
 
-// Function that makes requuist to twitter API
+
+// Function that makes request to twitter API
 
 function getTweet () {
 
-
+    var client = new Twitter(keys.twitter);
+      
+       
+      var params = {screen_name: 'abdiazizjama3'};
+      client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+          
+        for (var i =0; i < tweets.length; i++) {
+            console.log(tweets[i].text);
+        }
+        } 
+        // console.log(response);
+        
+        
+      });
 }
-
+getTweet();
 
 // Function that makes request to spotify
 
 function getSongs () {
+    
+    var spotify = new Spotify(keys.spotify);
+    
+     var songName = process.argv[3];
+
+    spotify.search({ type: 'track', query: songName, limit: 1 }, function(err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+     
+    console.log(data.tracks.items[0].artists[0].name); 
+    console.log(data.tracks.items[0].album.name);
+    console.log(data.tracks.items[0].name);
+    console.log(data.tracks.items[0].external_urls.spotify);
+    });
 
 
 }
 
+ if (process.argv[2] == "spotify-this-song") {
+ getSongs();
+
+}
 // Function that makes request to OMBD
 
-function getMovie () {
+// function getMovie () {
 
-
-}
+//     var omdbApi = require('omdb-client');
+ 
+//     var params = {
+//         apiKey: 'XXXXXXX',
+//         title: 'Terminator',
+//         year: 2012
+//     }
+//     omdbApi.get(params, function(err, data) {
+//         // process response...
+//     });
+// }
 
 // function for running a command from a texfile
 
