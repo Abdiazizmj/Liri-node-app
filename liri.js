c537868b3e4eff2acbdf812f3581e8f69b3e1efb
abdiaziz jama <abdiazizjama@er-10-132-12-47.wireless.umn.edu>
2018-04-02 14:10:21 -0500

    liri node

---
+++ liri.js
@@ -0,0 +1,51 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
+var req = require("request");
+
+// Function that makes requuist to twitter API
+
+function getTweet () {
+
+
+}
+
+
+// Function that makes request to spotify
+
+function getSongs () {
+
+
+}
+
+// Function that makes request to OMBD
+
+function getMovie () {
+
+
+}
+
+// function for running a command from a texfile
+
+function doWhatItSays () {
+
+}
+
+// function for determining what function to run
+
+function determineCommand () {
+
+}
+
+
+// function that takes in arguments and executes the correct function
+
+function runCommand () {
+
+}
+
+
+// execute function that takes in arguments and executes the command
+
+runCommand ();
