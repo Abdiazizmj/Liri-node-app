9535ef9724178892414b3a038beefdabce284f60
abdiaziz jama <abdiazizjama@abdiazizs-MacBook-Pro.local>
2018-04-02 21:47:11 -0500

    final file

--- liri.js
+++ liri.js
@@ -7,51 +7,52 @@ var fs = require('fs');
 
 // Function that makes request to twitter API
 
-function getTweet () {
+function getTweet() {
 
     var client = new Twitter(keys.twitter);
 
 
-      var params = {screen_name: 'abdiazizjama3'};
-      client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    var params = { screen_name: 'abdiazizjama3' };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
         if (!error) {
 
-        for (var i =0; i < tweets.length; i++) {
-            console.log(tweets[i].text);
+            for (var i = 0; i < tweets.length; i++) {
+                console.log(tweets[i].text);
+            }
         }
-        }
-      });
+    });
 }
-// getTweet();
+getTweet();
 
 // Function that makes request to spotify
 
-function getSongs () {
+function getSongs() {
 
     var spotify = new Spotify(keys.spotify);
 
-     var songName = process.argv[3];
+    var songName = process.argv[3];
 
-    spotify.search({ type: 'track', query: songName, limit: 1 }, function(err, data) {
-      if (err) {
-        return console.log('Error occurred: ' + err);
-      }
+    spotify.search({ type: 'track', query: songName, limit: 1 }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
 
-    console.log(data.tracks.items[0].artists[0].name);
-    console.log(data.tracks.items[0].album.name);
-    console.log(data.tracks.items[0].name);
-    console.log(data.tracks.items[0].external_urls.spotify);
+        console.log(data.tracks.items[0].artists[0].name);
+        console.log(data.tracks.items[0].album.name);
+        console.log(data.tracks.items[0].name);
+        console.log(data.tracks.items[0].external_urls.spotify);
     });
 
 
 }
 
- if (process.argv[2] == "spotify-this-song") {
- getSongs();
+if (process.argv[2] == "spotify-this-song") {
+    getSongs();
 }
+
 // Function that makes request to OMBD
 
-function getMovie () {
+function getMovie() {
 
     var omdbApi = require('omdb-client');
 
@@ -60,7 +61,7 @@ function getMovie () {
         title: 'Terminator',
         year: 2012
     }
-    omdbApi.get(params, function(err, data) {
+    omdbApi.get(params, function (err, data) {
         // process response...
     });
 }
@@ -68,8 +69,8 @@ function getMovie () {
 
 // function for running a command from a texfile
 
-function doWhatItSays () {
-    fs.readFile('random.txt', 'utf8', function(err, data) {
+function doWhatItSays() {
+    fs.readFile('random.txt', 'utf8', function (err, data) {
         if (err) throw err;
         console.log(data);
     });
@@ -77,8 +78,8 @@ function doWhatItSays () {
 
 // function for determining what function to run
 
-function determineCommand (command) {
-    switch(command) {
+function determineCommand(command) {
+    switch (command) {
         case `my-tweets`:
             console.log('get tweets')
             getTweet();
@@ -100,13 +101,13 @@ function determineCommand (command) {
     }
 }
 
-Array.prototype.remove = function(index) {
+Array.prototype.remove = function (index) {
     this.splice(index, 1);
 }
 
 // function that takes in arguments and executes the correct function
 
-function runCommand (command) {
+function runCommand(command) {
     command.shift();
     command.shift();
     // console.log(command)
@@ -119,4 +120,4 @@ function runCommand (command) {
 
 // execute function that takes in arguments and executes the command
 
-runCommand (process.argv);
+runCommand(process.argv);
